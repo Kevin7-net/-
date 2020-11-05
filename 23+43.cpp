f4d0b8ff5845c043314c5426828cc8d64f009b83
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-11-05 16:03:46 +0800

    Add files via upload

---
+++ 23+43.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("23+43=%d",23+43);
+	return 0;
+}
+
---
+++ 258+262.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<" 258 + 262 = "<<258+262;
+	return 0;
+}
