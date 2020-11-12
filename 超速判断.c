10bfee9af94f9a43d91e23b253f4f897139fd6c9
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-11-12 22:14:04 +0800

    Add files via upload

---
+++ 超速判断.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int i;
+	
+	scanf("%d",&i);
+	
+    if(i <= 60)
+    printf("Speed: %d - OK", i);
+	
+	
+	 else
+	 
+	 printf("Speed: %d -Speeding",i);
+	
+	return 0;
+}
