9d46f4615789bfcf587b40da1c0a18f98ef325e6
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-11-19 12:46:21 +0800

    Add files via upload

---
+++ 金字塔.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"           1         "<<endl;
+	cout<<"       1      3       "<<endl;
+	cout<<"   1       3      5"  <<endl;
+	cout<<"1      3      5      7"<<endl;	
+ } 
