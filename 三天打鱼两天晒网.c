1201a7c111936612b3c6b51fe21e8a1a198d463a
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-12-01 23:44:29 +0800

    Add files via upload

---
+++ 三天打鱼两天晒网.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int x;
+	
+	scanf("%d",&x);
+	
+	if(x%5<=3 && x%5!=0)
+	
+	
+	printf("Fishing in day %d",x);
+	
+	else
+	
+	printf("Drying in day %d",x);
+	
+	return 0;
+}
