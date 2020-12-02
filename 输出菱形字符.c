8cf33e520531012a4f41dfcfccffb9378ca7de3b
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-12-02 20:55:47 +0800

    Add files via upload

---
+++ 输出菱形字符.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	char n;
+	scanf("%c",&n);
+	printf("  %c  \n",n);
+	printf(" %c%c%c \n",n,n,n);
+	printf("%c%c%c%c%c\n",n,n,n,n,n);
+	printf(" %c%c%c \n",n,n,n);
+	printf("  %c  \n",n);
+	return 0;
+}
