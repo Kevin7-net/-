f4ed10ee0f213dc7b19a163c7eaa7da8122fbe9a
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-12-10 22:23:10 +0800

    Add files via upload

---
+++ 求平均分.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	double a,b,c,p;
+	cin>>a>>b>>c;
+	p=(a+b+c)/3;
+	cout<<fixed<<setprecision(2)<<p;
+	return 0;
+}
