06327f437fc726c1ca424f0022fbe7da8b892ad1
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-12-14 18:19:13 +0800

    Add files via upload

---
+++ 两个整数的四则运算.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num1,num2;
+	int a,b,c,d,e;
+	cin>>num1>>num2; 
+	a=num1+num2;
+	b=num1-num2;
+	c=num1*num2;
+	if(num2!=0)
+	d=num1/num2;
+	e=num1%num2;
+	cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e;
+	return 0;
+ } 
