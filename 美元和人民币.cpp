b2570f1d82bbb785c20473953b31be3b3a1b2ee8
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2020-12-15 22:16:10 +0800

    Add files via upload

---
+++ 美元和人民币.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	double dollar,rmb;
+	cin>>dollar;
+	rmb=dollar*6.5573;
+	cout<<fixed<<setprecision(2)<<rmb<<endl;
+	return 0;
+ } 
