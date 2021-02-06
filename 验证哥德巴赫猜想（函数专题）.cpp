d9220f09ae0da101ddc581075a52673b44f4a37f
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-02-06 22:00:13 +0800

    Add files via upload

---
+++ 验证哥德巴赫猜想（函数专题）.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int prime(int a);
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=2;i<=n;i++){
+		int y=prime(i);
+		if(y==1){
+			for(int j=i;j<=n;j++){
+				if(i+j==n&&prime(j)==1){
+					cout<<i<<" "<<j<<endl;
+				}
+			}
+		} 
+	}
+	return 0;
+}
+
+int prime(int a){
+	for(int i=2;i<=sqrt(a);i++){//ÖØµãi<=sqrt(a)
+		if(a%i==0){
+		return 0;
+		}
+	}
+	return 1;
+	
+}
