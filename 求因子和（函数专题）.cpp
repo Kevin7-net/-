99e4e1176ed4907d39eefff938b97d6ef907b61e
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-02-19 23:03:15 +0800

    Add files via upload

---
+++ 求因子和（函数专题）.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int FacSum(int n);
+
+int FacSum(int n)
+{
+	int sum=0;
+	for(int i=1;i<n;i++){
+		if(n%i==0){
+			sum+=i;
+		}
+	}
+	return sum;
+}
+int main()
+{
+	int n,m;
+	cin>>n;
+	m=FacSum(n);
+	cout<<m;
+	return 0;
+}
