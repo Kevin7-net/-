c6d253249b382b02ba77d6d92348908be768ff37
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-02-23 14:45:41 +0800

    Add files via upload

---
+++ 逆序.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void inverse(int n)
+{
+	int i=0;
+	if(n==0)
+	return;
+	cin>>i;
+	inverse(--n);
+	cout<<setw(4)<<i;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	inverse(n);
+}
