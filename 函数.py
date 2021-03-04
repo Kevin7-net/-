b8e93e824d5d1bc63120bf05201b3893fd09ebe8
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-03-04 23:05:33 +0800

    Add files via upload

---
+++ 函数.py
@@ -0,0 +1,37 @@
+# -*- coding = utf-8 -*-
+# @Time : 2021/3/2 15:15
+# Author : Eric
+# @File : 函数.py
+# @Software: PyCharm
+
+'''
+def hx():
+    print("--------")
+hx()
+'''
+
+'''
+def hx(a):
+    print("-" * a)
+
+
+hx(5)
+'''
+
+'''
+def jia(a, b, c):
+    d = a + b + c
+    print(d)
+
+
+jia(1, 2, 3)
+'''
+
+'''
+def ping(a, b, c):
+    d = (a + b + c) / 3
+    print(d)
+
+
+ping(1, 2, 5)
+'''
