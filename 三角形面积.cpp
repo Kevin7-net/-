69d251aed5bb15b328094f7e5c5802c3bf83f9f3
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-03-15 22:40:13 +0800

    Add files via upload

---
+++ 三角形面积.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	double a,b,c,s,p;
+	cin>>a>>b>>c;
+	p=0.5*(a+b+c);
+	s=sqrt(p*(p-a)*(p-b)*(p-c));
+	cout<<fixed<<setprecision(1)<<s;
+	return 0;
+ } 
