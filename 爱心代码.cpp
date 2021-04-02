67d94b9421eb2d4e779e3c84c410371830b6540b
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-04-02 20:16:27 +0800

    Add files via upload

---
+++ 爱心代码.cpp
@@ -0,0 +1,22 @@
+//°®ÐÄ´úÂë
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float y=1.5f;
+	
+	for(;y > -1.5f;y-= 0.1f) {
+	
+	float x=-1.5f;
+	
+	for (;x< 1.5f;x += 0.05f) {
+	
+	float a=x*x+y*y-1;
+	
+	cout<<((a *a*a-x*x*y*y*y <= 0.0f)?'*' : ' ');
+	}
+	cout<<endl;
+	}
+	return 0;
+}
