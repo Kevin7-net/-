8e4c91cd521e1854a8a489b55c3eb7f76d3a5ca3
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-04-10 23:31:02 +0800

    Add files via upload

--- 最近共同祖先（函数专题）.cpp
+++ 最近共同祖先（函数专题）.cpp
@@ -1,24 +1,54 @@
+//#include<iostream>
+//using namespace std;
+//int common(int x,int y);
+//
+//int common(int x,int y)
+//{
+//	if(x==y){
+//		return x;
+//	}
+//	else if(x>y){
+//		common(x/2,y);
+//	}
+//	else{
+//		common(x,y/2);
+//	}
+//}
+//int main()
+//{
+//	int x,y,z;
+//	cin>>x>>y;
+//	z=common(x,y);
+//	cout<<z;
+//	return 0;
+//}
 #include<iostream>
 using namespace std;
-int common(int x,int y);
 
-int common(int x,int y)
+int common(int x,int y,int z)
 {
-	if(x==y){
+	if(x==y&&x==z){
 		return x;
 	}
-	else if(x>y){
-		common(x/2,y);
+	else if(x>=y&&x>=z){
+		common(x/3,y,z);
 	}
-	else{
-		common(x,y/2);
+	else if(y>=x&&y>=z){
+		common(x,y/3,z);
 	}
+    else if(z>=x&&z>=y){
+        common(x,y,z/3);
+    }
 }
 int main()
 {
-	int x,y,z;
-	cin>>x>>y;
-	z=common(x,y);
-	cout<<z;
-	return 0;
+    int n,a[100],b[100],c[100];
+    cin>>n; 
+    for(int i=0;i<n;i++){
+        cin>>a[i]>>b[i]>>c[i];
+      }
+    for(int i=0;i<n;i++){
+      	cout<<common(a[i],b[i],c[i])<<endl;
+	}
+    return 0;
 }
