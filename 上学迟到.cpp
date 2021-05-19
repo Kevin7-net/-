5c62e11f2ab1c1420a6a7ae607e90c72315b45ef
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-05-19 16:57:17 +0800

    Add files via upload

---
+++ 上学迟到.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	double s,v,m;
+	int n,t,a,b;
+	n=8*60+24*60;
+	cin>>s>>v;
+	t=ceil(s/v)+10;
+	n-=t;
+	if(n>=24*60){
+		n-=24*60;
+	}
+	a=n/60;
+	b=n%60;
+	if(a<10)//ÂýÂýÅÐ¶ÏÊÇ·ñ²¹0
+	{
+		if(b<10) cout<<"0"<<a<<":0"<<b;
+		else cout<<"0"<<a<<":"<<b;
+	}
+	else
+	{
+		if(b<10) cout<<a<<":0"<<b;
+		else cout<<a<<":"<<b;
+	}
+	return 0;
+ } 
