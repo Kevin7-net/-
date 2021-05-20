7c2e77f64b1033a02f614f58204a0b0fa8e37f4f
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-05-20 16:20:12 +0800

    Add files via upload

---
+++ 杨辉三角输出.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[31][31];
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			a[i][1]=1;
+			a[i][i]=1;
+		}
+	}
+	for(int i=3;i<=n;i++){
+		for(int j=2;j<i;j++){
+			a[i][j]=a[i-1][j-1]+a[i-1][j];
+		}
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
