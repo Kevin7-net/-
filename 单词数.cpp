a73e6752b4d08a6f71fcc06cfb5c896d9b936c33
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-07-03 22:19:27 +0800

    Add files via upload

---
+++ 单词数.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring> 
+using namespace std;
+
+int main()
+{
+	char s[100];
+	int sum=0,i=0;
+	while(scanf("%s",&s)!='#'){
+		
+	}
+	cout<<sum<<endl;
+	return 0;
+ } 
