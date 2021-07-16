467e54a5d46b1a1cf94f0d02175a8fb98bc261c8
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-07-16 17:18:26 +0800

    Add files via upload

---
+++ 谁去要微信 5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long yy=0;
+	long long x,y,z;
+	cin>>x>>y>>z;
+	if(x+y==z){
+		cout<<"yandi";
+	}
+	else{
+		cout<<"didi";
+	}
+	return 0;
+}
