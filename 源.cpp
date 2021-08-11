4e6d6c7953aa4230f99ad5125b16a43348889881
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-08-11 22:03:23 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,43 +1,27 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 
-int zh(int n)
-{
-	int s;
-	if (n > 4)
-		s=n-1;
-	else
-		s = n;
-	return s;
-}
-//由题可知，跳过了数字4，所以里程表显示的是九进制的数，只需将九进制化为十进制就可以解出。
 int main()
 {
-	int num,k=0,n,a,cnt=0;
-	cin >> num;
-	n = num;
-	while (n > 0) {
-		a = n % 10;
-		n /= 10;
-		a = zh(a);
-		cnt += a*pow(9, k++);
+	int n, sum = 0, max = 0, a, m;
+	cin >> n;
+	while (n--) {
+		sum = 0;
+		max = 0;
+		cin >> m;
+		for (int i = 0; i < m; i++) {
+			cin >> a;
+			sum += a;
+			if (max < a) {
+				max = a;
+			}
+		}
+		if (max - 1 <= sum - max) {
+			cout << "Yes" << endl;
+		}
+		else {
+			cout << "No" << endl;
+		}
 	}
-	cout << cnt;
 	return 0;
 }
-//#include<iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int num,cnt=0;
-//	cin >> num;
-//	for (int i = 1; i <= num; i++) {
-//		if (i % 10 != 4 && i / 10 % 10 != 4 && i / 100 % 10 != 4) {
-//			cnt++;
-//		}
-//	}
-//	cout << cnt;
-//	return 0;
-//}	
