0f1606c0ac15815614d9ff64573c7a7d9c99101f
Kevin7-net <73951688+Kevin7-net@users.noreply.github.com>
2021-08-11 22:03:46 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -3,25 +3,35 @@ using namespace std;
 
 int main()
 {
-	int n, sum = 0, max = 0, a, m;
-	cin >> n;
-	while (n--) {
-		sum = 0;
-		max = 0;
-		cin >> m;
-		for (int i = 0; i < m; i++) {
-			cin >> a;
-			sum += a;
-			if (max < a) {
-				max = a;
-			}
-		}
-		if (max - 1 <= sum - max) {
-			cout << "Yes" << endl;
-		}
-		else {
-			cout << "No" << endl;
-		}
-	}
-	return 0;
+    int m, n;
+    int a[101], b[102];
+    int i, j;
+    int flag = 0;
+    cin >> n >> m;
+
+    for (i = 0; i < n; i++)
+        cin >> a[i];
+    for (j = 0; j < m; j++)
+        cin >> b[j];
+    for (i = 0; i < n - m + 1; i++)//只需要查找子数组的长度就行了
+    {
+        for (j = 0; j < m; j++)
+        {
+            if (a[i + j] != b[j])
+                break;
+        }
+        if (j == m)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+
+    if (flag == 1)
+        cout << i;
+    else
+        cout << "No Answer" << endl;
+
+    return 0;
 }
